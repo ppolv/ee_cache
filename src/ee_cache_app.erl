64efe973fe78cbce62afde08901b39f5a9b0337f
Pablo Polvorin <pablo.polvorin@process-one.net>
2013-06-11 17:34:53 -0300

    Fix for older erlang releases
    when application:get_env/3 isn't available

--- src/ee_cache_app.erl
+++ src/ee_cache_app.erl
@@ -12,7 +12,10 @@
  %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-	EvictEach = application:get_env(ee_cache, evict_each, 10), %%default is evict each 10 seconds
+	EvictEach = case application:get_env(ee_cache, evict_each) of %%default is evict each 10 seconds
+			undefined -> 10;
+			N -> N
+		end,
 	ee_cache_sup:start_link(EvictEach).
 
 stop(_State) ->
